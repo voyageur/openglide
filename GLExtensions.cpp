c91fc43633777076403570d665503e2e61618b93
paul <>
2001-06-28 10:45:52 +0000

    Hard-wire InternalConfig.PaletteEXTEnable off; screws up textures in TR I

--- GLExtensions.cpp
+++ GLExtensions.cpp
@@ -140,7 +140,7 @@ void ValidateUserConfig()
 	{
 		if ( isExtensionSupported( "GL_EXT_shared_texture_palette" ) )
 		{
-			InternalConfig.PaletteEXTEnable			= true;
+			InternalConfig.PaletteEXTEnable			= false;
 		}
 	}
 
