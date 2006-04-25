da3f96be7a49d088c587299d9564446d406fdbc9
s_a_white <>
2006-04-25 19:29:28 +0000

    Moved more functions from stubs to wrappers.

--- platform/dosemu/glide2x.c
+++ platform/dosemu/glide2x.c
@@ -20,132 +20,7 @@ struct fptr_##type##_t \
 }
 
 /*******************************************************************************************************/
-#ifdef DOSEMU
-
-#include "config.h"
-#include "cpu.h"
-
-extern unsigned long SEL_ADR(unsigned short sel, unsigned long reg);
-
-
-#define THUNK(func) THUNK_##func
-
-#define ENDDECLARE }}
-
-#define DECLARE_THUNK0(func,ret) \
-static void THUNK(func)(struct sigcontext *scp) { \
-
-#define DECLARE_THUNK1(func,ret,t1,v1) \
-static void THUNK(func)(struct sigcontext *scp) { \
-    t1 v1; \
-    {   unsigned long *stack = (unsigned long *)SEL_ADR(_ss, _esp) + 2; \
-        v1 = (t1) DWORD(stack[0]); \
-    }
-
-#define DECLARE_THUNK2(func,ret,t1,v1,t2,v2) \
-static void THUNK(func)(struct sigcontext *scp) { \
-    t1 v1; t2 v2; \
-    {   unsigned long *stack = (unsigned long *)SEL_ADR(_ss, _esp) + 2; \
-        v1 = (t1) DWORD(stack[0]); \
-        v2 = (t2) DWORD(stack[1]); \
-    }
-
-#define DECLARE_THUNK3(func,ret,t1,v1,t2,v2,t3,v3) \
-static void THUNK(func)(struct sigcontext *scp) { \
-    t1 v1; t2 v2; t3 v3; \
-    {   unsigned long *stack = (unsigned long *)SEL_ADR(_ss, _esp) + 2; \
-        v1 = (t1) DWORD(stack[0]); \
-        v2 = (t2) DWORD(stack[1]); \
-        v3 = (t3) DWORD(stack[2]); \
-    }
-
-#define DECLARE_THUNK4(func,ret,t1,v1,t2,v2,t3,v3,t4,v4) \
-static void THUNK(func)(struct sigcontext *scp) { \
-    t1 v1; t2 v2; t3 v3; t4 v4; \
-    {   unsigned long *stack = (unsigned long *)SEL_ADR(_ss, _esp) + 2; \
-        v1 = (t1) DWORD(stack[0]); \
-        v2 = (t2) DWORD(stack[1]); \
-        v3 = (t3) DWORD(stack[2]); \
-        v4 = (t4) DWORD(stack[3]); \
-    }
-
-#define DECLARE_THUNK5(func,ret,t1,v1,t2,v2,t3,v3,t4,v4,t5,v5) \
-static void THUNK(func)(struct sigcontext *scp) { \
-    t1 v1; t2 v2; t3 v3; t4 v4; t5 v5; \
-    {   unsigned long *stack = (unsigned long *)SEL_ADR(_ss, _esp) + 2; \
-        v1 = (t1) DWORD(stack[0]); \
-        v2 = (t2) DWORD(stack[1]); \
-        v3 = (t3) DWORD(stack[2]); \
-        v4 = (t4) DWORD(stack[3]); \
-        v5 = (t5) DWORD(stack[4]); \
-    }
-
-#define DECLARE_THUNK6(func,ret,t1,v1,t2,v2,t3,v3,t4,v4,t5,v5,t6,v6) \
-static void THUNK(func)(struct sigcontext *scp) { \
-    t1 v1; t2 v2; t3 v3; t4 v4; t5 v5; t6 v6; \
-    {   unsigned long *stack = (unsigned long *)SEL_ADR(_ss, _esp) + 2; \
-        v1 = (t1) DWORD(stack[0]); \
-        v2 = (t2) DWORD(stack[1]); \
-        v3 = (t3) DWORD(stack[2]); \
-        v4 = (t4) DWORD(stack[3]); \
-        v5 = (t5) DWORD(stack[4]); \
-        v6 = (t6) DWORD(stack[5]); \
-    }
-
-#define DECLARE_THUNK7(func,ret,t1,v1,t2,v2,t3,v3,t4,v4,t5,v5,t6,v6,t7,v7) \
-static void THUNK(func)(struct sigcontext *scp) { \
-    t1 v1; t2 v2; t3 v3; t4 v4; t5 v5; t6 v6; t7 v7; \
-    {   unsigned long *stack = (unsigned long *)SEL_ADR(_ss, _esp) + 2; \
-        v1 = (t1) DWORD(stack[0]); \
-        v2 = (t2) DWORD(stack[1]); \
-        v3 = (t3) DWORD(stack[2]); \
-        v4 = (t4) DWORD(stack[3]); \
-        v5 = (t5) DWORD(stack[4]); \
-        v6 = (t6) DWORD(stack[5]); \
-        v7 = (t7) DWORD(stack[6]); \
-    }
-
-#define DECLARE_THUNK8(func,ret,t1,v1,t2,v2,t3,v3,t4,v4,t5,v5,t6,v6,t7,v7,t8,v8) \
-static void THUNK(func)(struct sigcontext *scp) { \
-    t1 v1; t2 v2; t3 v3; t4 v4; t5 v5; t6 v6; t7 v7; t8 v8; \
-    {   unsigned long *stack = (unsigned long *)SEL_ADR(_ss, _esp) + 2; \
-        v1 = (t1) DWORD(stack[0]); \
-        v2 = (t2) DWORD(stack[1]); \
-        v3 = (t3) DWORD(stack[2]); \
-        v4 = (t4) DWORD(stack[3]); \
-        v5 = (t5) DWORD(stack[4]); \
-        v6 = (t6) DWORD(stack[5]); \
-        v7 = (t7) DWORD(stack[6]); \
-        v8 = (t8) DWORD(stack[7]); \
-    }
-
-#define DECLARE_THUNK15(func,ret,t1,v1,t2,v2,t3,v3,t4,v4,t5,v5,t6,v6,t7,v7,t8,v8,t9,v9,t10,v10,t11,v11,t12,v12,t13,v13,t14,v14,t15,v15) \
-static void THUNK(func)(struct sigcontext *scp) { \
-    t1 v1; t2 v2; t3 v3; t4 v4; t5 v5; t6 v6; t7 v7; t8 v8; t9 v9; \
-    t10 v10; t11 v11; t12 v12; t13 v13; t14 v14; t15 v15; \
-    {   unsigned long *stack = (unsigned long *)SEL_ADR(_ss, _esp) + 2; \
-        v1 = (t1) DWORD(stack[0]); \
-        v2 = (t2) DWORD(stack[1]); \
-        v3 = (t3) DWORD(stack[2]); \
-        v4 = (t4) DWORD(stack[3]); \
-        v5 = (t5) DWORD(stack[4]); \
-        v6 = (t6) DWORD(stack[5]); \
-        v7 = (t7) DWORD(stack[6]); \
-        v8 = (t8) DWORD(stack[7]); \
-        v9 = (t9) DWORD(stack[8]); \
-        v10 = (t10) DWORD(stack[9]); \
-        v11 = (t11) DWORD(stack[10]); \
-        v12 = (t12) DWORD(stack[11]); \
-        v13 = (t13) DWORD(stack[12]); \
-        v14 = (t14) DWORD(stack[13]); \
-        v15 = (t15) DWORD(stack[14]); \
-    }
-
-#define RETURNI(v) _eax = v;
-
-
-/*******************************************************************************************************/
-#else /* WRAPPERS DJGPP */
+/* WRAPPERS */
 
 #define __faddr(addr) asm volatile ("mov %1, %%eax; add %%eax, %0": "=g"(addr): "m"(fptr.offset): "%eax")
 
@@ -297,7 +172,6 @@ static void *entry (unsigned int cmd, unsigned short *segment, unsigned int *off
 }
 
 /*******************************************************************************************************/
-#endif /* END WRAPPERS */
 
 #include "thunks.i"
 
@@ -336,33 +210,6 @@ DECLARE_STUB6(_GRTEXDOWNLOAD_DEFAULT_32_WIDES, void, int, a, int, b, int, c, int
 ENDDECLARE
 
 DECLARE_STUB5(GRSPLASH, void, int, a, int, b, int, c, int, d, int, e)
-ENDDECLARE
-
-DECLARE_STUB2(GU3DFGETINFO, FxBool, char*, filename, Gu3dfInfo*, file_info)
-    /*
-     * Pretend file doesn't exist to get TRI to
-     * generate textures on the fly.
-     * FIXME: wont work on most games, probably
-     */
-    return FXFALSE;
-/*
-    FxBool ret;
-    __faddr (filename);
-    __faddr (file_info);
-    ret = __fcall (filename, file_info);
-    RETURNI(ret);
-*/
-ENDDECLARE
-
-DECLARE_STUB2(GU3DFLOAD, FxBool, char*, filename, Gu3dfInfo*, file_info)
-    return FXFALSE;
-/*
-    FxBool ret;
-    __faddr (filename);
-    __faddr (file_info);
-    ret = __fcall (filename, file_info);
-    RETURNI(ret);
-*/
 ENDDECLARE
 
 DECLARE_STUB1(GUENDIANSWAPWORDS, void, int, a)
@@ -407,20 +254,6 @@ ENDDECLARE
 DECLARE_STUB6(GUFBWRITEREGION, void, int, a, int, b, int, c, int, d, int, e, int, f)
 ENDDECLARE
 
-DECLARE_STUB1(GRGLIDEGETVERSION, void, char*, version)
-    memcpy(version, "2.48.00.0455", 13);
-ENDDECLARE
-
-DECLARE_STUB1(GRGLIDEGETSTATE, void, int, a)
-ENDDECLARE
-
-DECLARE_STUB1(_GRGLIDEINIT, void, int, version)
-#warning TODO
-ENDDECLARE
-
-DECLARE_STUB1(GRGLIDESHAMELESSPLUG, void, int, a)
-ENDDECLARE
-
 DECLARE_STUB0(GRRESETTRISTATS, void)
 ENDDECLARE
 
@@ -439,69 +272,7 @@ ENDDECLARE
 DECLARE_STUB2(GRSSTVIDMODE, void, int, a, int, b)
 ENDDECLARE
 
-DECLARE_STUB4(GRTEXCALCMEMREQUIRED, FxU32, GrLOD_t, lodmin, GrLOD_t, lodmax, GrAspectRatio_t, aspect, GrTextureFormat_t, fmt)
-#if 0
-    int size, i;
-
-    size = 0;
-
-    for(i = lodmax; i <= lodmin; i++)
-        size += texture_size(i, aspect, fmt);
-
-    DPRINTF("size = %x\n", size);
-
-    return size;
-#endif
-    return 0;
-ENDDECLARE
-
-/* FIXME? */
-DECLARE_STUB4(GRTEXDETAILCONTROL, void, int, a, int, b, int, c, int, d)
-#if 0
-    send_char(CMD_GrTexDetailControl);
-#endif
-ENDDECLARE
-
-/* FIXME? */
-DECLARE_STUB1(GRTEXMINADDRESS, FxU32, int, a)
-    return 8;
-ENDDECLARE
-
-/* FIXME? */
-DECLARE_STUB1(GRTEXMAXADDRESS, FxU32, int, a)
-    return 8*1024*1024 - 128*1024;
-ENDDECLARE
-
-/* FIXME */
-DECLARE_STUB2(GRTEXTEXTUREMEMREQUIRED, int, FxU32, evenOdd, GrTexInfo*, info)
-    return 0;
-ENDDECLARE
-
 DECLARE_STUB5(GRTEXDOWNLOADTABLEPARTIAL, void, int, a, int, b, int, c, int, d, int, e)
-ENDDECLARE
-
-/* FIXME? */
-DECLARE_STUB8(GRTEXDOWNLOADMIPMAPLEVEL, void,
-                                        GrChipID_t,        tmu,
-                                        FxU32,             startAddress,
-                                        GrLOD_t,           thisLod,
-                                        GrLOD_t,           largeLod,
-                                        GrAspectRatio_t,   aspectRatio,
-                                        GrTextureFormat_t, format,
-                                        FxU32,             evenOdd,
-                                        void*,             data)
-    GrTexInfo info;
-
-    if(thisLod == largeLod)
-    {
-        info.aspectRatio = aspectRatio;
-        info.data        = data;
-        info.format      = format;
-        info.largeLod    = largeLod;
-        info.smallLod    = largeLod;
-
-        GRTEXDOWNLOADMIPMAP(tmu, startAddress, evenOdd, &info);
-    }
 ENDDECLARE
 
 DECLARE_STUB1(GRERRORSETCALLBACK, void, int, a)
@@ -521,11 +292,6 @@ DECLARE_STUB12(GUTEXCHANGEATTRIBUTES, void, int, a, int, b, int, c, int, d,
                                       int, i, int, j, int, k, int, l)
 ENDDECLARE
 
-/* FIXME */
-DECLARE_STUB2(GUTEXCOMBINEFUNCTION, void, int, a, int, b)
-/*    __fcall (a, b); */
-ENDDECLARE
-
 DECLARE_STUB3(GUTEXDOWNLOADMIPMAPLEVEL, void, int, a, int, b, int, c)
 ENDDECLARE
 
@@ -533,11 +299,6 @@ DECLARE_STUB1(GUTEXGETCURRENTMIPMAP, void, int, a)
 ENDDECLARE
 
 DECLARE_STUB1(GUTEXGETMIPMAPINFO, void, int, a)
-ENDDECLARE
-
-/* FIXME */
-DECLARE_STUB1(GUTEXMEMQUERYAVAIL, FxU32, int, a)
-    return 16 * 1024 * 1024;
 ENDDECLARE
 
 DECLARE_STUB1(_GUMPTEXCOMBINEFUNCTION, void, int, a)
@@ -579,11 +340,6 @@ ENDDECLARE
 DECLARE_STUB1(GRDITHERMODE, void, int, a)
 ENDDECLARE
 
-/* FIXME? */
-DECLARE_STUB0(GRBUFFERNUMPENDING, int)
-    return 0;
-ENDDECLARE
-
 DECLARE_STUB1(GRGLIDESETSTATE, void, int, a)
 ENDDECLARE
 
@@ -603,35 +359,6 @@ DECLARE_STUB1(GRLFBCONSTANTDEPTH, void, int, a)
 ENDDECLARE
 
 DECLARE_STUB1(_GRCOLORCOMBINEDELTA0MODE, void, int, a)
-ENDDECLARE
-
-/* FIXME! */
-DECLARE_STUB2(GRLFBUNLOCK, FxBool, GrLock_t, type, GrBuffer_t, buffer)
-#if 0
-    if((type & 1) == 1)
-    {
-       /*
-        * If we are delaying LFB writes and some triangles
-        * have been drawn this frame, then just mark that
-        * there are writes yet to be sent.  If no triangles
-        * have yet been drawn then the purpose of the writes
-        * is probably to create a background so send them now.
-        */
-        if(delay_lfb_write && frame_started)
-        {
-            lfb_write_pending = 1;
-        }
-
-        else
-        {
-            send_blue_screen(buffer);
-            buffer_is_locked = 0;
-        }
-
-    }
-
-#endif
-    return FXTRUE;
 ENDDECLARE
 
 DECLARE_STUB1(GRLFBWRITECOLORFORMAT, void, int, a)
@@ -655,11 +382,6 @@ DECLARE_STUB1(GRSSTPERFSTATS, void, int, a)
 ENDDECLARE
 
 DECLARE_STUB0(GRSSTRESETPERFSTATS, void)
-ENDDECLARE
-
-/* FIXME */
-DECLARE_STUB0(GRSSTSTATUS, int)
-    return 0x0FFFF43F;
 ENDDECLARE
 
 DECLARE_STUB0(GRSSTVIDEOLINE, void)
