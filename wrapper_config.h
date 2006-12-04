cd5879e2c574bb276140c6d64da2d315a6fd9e96
glidos <>
2006-12-04 11:29:29 +0000

    New config option used only by psVoodoo

--- wrapper_config.h
+++ wrapper_config.h
@@ -6,6 +6,7 @@
 #define WRAPPER_FLAG_WINDOWED               (0x1)
 #define WRAPPER_FLAG_MIPMAPS                (0x2)
 #define WRAPPER_FLAG_PALETTED_TEXTURE_CACHE (0x4)
+#define WRAPPER_FLAG_MANAGED_TEXTURES       (0x8)
 
 FX_ENTRY void FX_CALL setConfig(FxU32 flags);
 
